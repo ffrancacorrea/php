5e6b17012a5e72a1ff28886ef40f33d1e57f314d
ffrancacorrea <45070469+ffrancacorrea@users.noreply.github.com>
2018-11-15 15:47:02 +0100

    Update read.php

--- read.php
+++ read.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style/read.css">
+    <link rel="stylesheet" href="read.css">
     <title>INFO</title>
 </head>
 <body>
